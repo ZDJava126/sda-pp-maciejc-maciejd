026819e697c50ec07d100f854b7087bec896d6f7
MaciekC <inz.maciej.cichonski@gmail.com>
2023-01-15 12:26:57 +0100

    Poprawione 13

--- src/main/java/com/sda/model/User.java
+++ src/main/java/com/sda/model/User.java
@@ -3,10 +3,15 @@ package com.sda.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @NoArgsConstructor
 @Entity
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+
 
 public class User {
     @Id
@@ -23,62 +28,6 @@ public class User {
     @Column(name = "E-MAIL")
     private String email;
 
-    public User(String username, String password, String name, String surname, int age, String email) {
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Override
     public String toString() {
