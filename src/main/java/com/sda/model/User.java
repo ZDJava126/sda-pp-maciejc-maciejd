59154e0fc4fb1dd6b723bad24699797c11cc4e9e
MaciekC <inz.maciej.cichonski@gmail.com>
2023-01-15 12:55:33 +0100

    Poprawione 13v3 dodany import

--- src/main/java/com/sda/model/User.java
+++ src/main/java/com/sda/model/User.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
 
 @NoArgsConstructor
 @Entity
